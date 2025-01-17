c0aacdc72edca73a7a20db1edf3439a6339eaca9
Giorgixmaladze <118875602+Giorgixmaladze@users.noreply.github.com>
2025-01-17 23:25:45 +0400

    commit

--- day151/homework/calculator app/src/components/calc.jsx
+++ day151/homework/calculator app/src/components/calc.jsx
@@ -4,62 +4,101 @@ import { useState, useEffect } from "react";
 function Calculator() {
     const [firstNumber, setFirstnumber] = useState("")
     const [secondNumber, setSecondnumber] = useState("")
-    const [op, setOp] = useState([])
+    const [op, setOp] = useState("")
 
 
-    useEffect(() => {
-        if (op.length === 1 && op[0] === "=") {
-            setOp([])
-        } else if (firstNumber.length === 2) {
-            if (op[0] === "+") {
-                setFirstnumber(Number(firstNumber + secondNumber))
-            } else if (op[0] === "-") {
-                setFirstnumber(Number(firstNumber - secondNumber))
-            } else if (op[0] === "*") {
-                setFirstnumber(Number(firstNumber * secondNumber))
-            } else if (op[0] === "/") {
-                setFirstnumber(secondNumber === "0" ? "Error" : firstNumber / secondNumber)
-            }
-            secondNumber("")
-            op[1] !== "=" ? setOp(prev => [prev[1]]) : setOp([])
-        }
-    }, [op])
+    // useEffect(() => {
+    //     if (op.length === 1 && op[0] === "=") {
+    //         setOp([])
+    //     } else if (firstNumber.length === 2) {
+    //         if (op[0] === "+") {
+    //             setFirstnumber(Number(firstNumber + secondNumber))
+    //         } else if (op[0] === "-") {
+    //             setFirstnumber(Number(firstNumber - secondNumber))
+    //         } else if (op[0] === "*") {
+    //             setFirstnumber(Number(firstNumber * secondNumber))
+    //         } else if (op[0] === "/") {
+    //             setFirstnumber(secondNumber === "0" ? "Error" : firstNumber / secondNumber)
+    //         }
+    //         secondNumber("")
+    //         op[1] !== "=" ? setOp(prev => [prev[1]]) : setOp([])
+    //     }
+    // }, [op])
+
+
 
 
 
 
     const handleClick = (item) => {
-        setFirstnumber(prev => prev + item)
+        // setFirstnumber(prev => prev + item)
+        // if (item === "reset") {
+        //     setFirstnumber("")
+        //     setSecondnumber("")
+        //     setOp([])
+        // } else if (item === "del") {
+        //     secondNumber === "" ? setFirstnumber("") : setSecondnumber("")
+        // } else if (typeof item !== "number" && item !== ".") {
+        //     setOp(prev => {
+        //         return prev.length === 1 && prev[0] === item && secondNumber === "" ? [item] : [...prev, item]
+        //     })
+
+        // } else {
+        //     if (op.length < 1) {
+
+        //         if (item === "." && !firstNumber.includes(item) || typeof item === "number") {
+        //             setFirstnumber(prev => prev + item)
+
+        //         }
+
+        //     } else {
+        //         if (item === "." && !secondNumber.includes(item) || typeof item === "number") {
+        //             setSecondnumber(prev => prev + item)
+
+        //         }
+        //     }
+        // }
         if (item === "reset") {
-            setFirstnumber("")
-            setSecondnumber("")
-            setOp([])
+            setFirstnumber("");
+            setSecondnumber("");
+            setOp("");
         } else if (item === "del") {
-            secondNumber === "" ? setFirstnumber("") : setSecondnumber("")
-        } else if (typeof item !== "number" && item !== ".") {
-            setOp(prev => {
-                return prev.length === 1 && prev[0] === item && secondNumber === "" ? [item] : [...prev, item]
-            })
-
-        } else {
-            if (op.length < 1) {
-
-                if (item === "." && !firstNumber.includes(item) || typeof item === "number") {
-                    setFirstnumber(prev => prev + item)
-
-                }
-
+            if (secondNumber) {
+                setSecondnumber(secondNumber.slice(0, -1));
+            } else if (op) {
+                setOp("");
             } else {
-                if (item === "." && !secondNumber.includes(item) || typeof item === "number") {
-                    setSecondnumber(prev => prev + item)
+                setFirstnumber(firstNumber.slice(0, -1));
+            }
+        } else if (item === "=") {
+            let result;
+            const num1 = parseFloat(firstNumber);
+            const num2 = parseFloat(secondNumber);
 
-                }
+            if(op === "+"){
+                result = num1 + num2
+            }else if(op ==="-"){
+                result = num1 - num2
+            }else if(op === "*"){
+                result = num1 * num2
+            }else if(op === "/"){
+                result = num1 !== 0 ? num1 / num2 : "Error"
+            }
+            setFirstnumber(result.toString());
+        
+            setSecondnumber("");
+            setOp("");
+        } else if (["+", "-", "*", "/"].includes(item)) {
+            setOp(item);
+        } else {
+            if (op === "") {
+                setFirstnumber(firstNumber + item);
+            } else {
+                setSecondnumber(secondNumber + item);
             }
         }
 
-        console.log(firstNumber)
-        console.log(secondNumber)
-        console.log(op)
+   
     }
 
     return (
@@ -91,7 +130,7 @@ function Calculator() {
                     <button className="nums" onClick={() => handleClick("*")}>x</button>
 
                     <button type="reset" id="reset" onClick={() => handleClick("reset")}>RESET</button>
-                    <button id="equal">=</button>
+                    <button id="equal" onClick={() => handleClick("=")}>=</button>
 
                 </div>
 
