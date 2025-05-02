84e3a01fb0767b320a48f2a9737f2a8d12f6bb68
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2025-05-02 21:58:32 +0400

    commit

---
+++ day175/classwork/Products/src/components/Products.jsx
@@ -0,0 +1,17 @@
+const Products = () =>{
+    
+    
+    
+    return(
+
+        <>
+            <h1>Products Page</h1>
+
+            <h2>Categories</h2>
+            <button>Vegetables</button>
+        </>
+    )
+
+
+
+}
---
+++ day175/classwork/Products/src/utils/type.js
@@ -0,0 +1,42 @@
+const category = (type) =>{
+    const vegetables = [
+        {
+            name:"Tomato",
+            price:"5.00",
+            
+        },
+        {
+            name:"Potato",
+            price:"5.00"
+        },
+        {
+            name:"Carrot",
+            price:"2.00"
+        },
+        {
+            name:"Cabbage",
+            price:"12.00"
+        }
+    ]
+
+    const fruits = [
+        {
+            name:"Melon",
+            price:"10.00"
+        },
+        {
+            name:"Watermelon",
+            price:"12.00"
+        },
+        {
+            name:"Apple",
+            price:"2.00"
+        },
+        {
+            name:"banana",
+            price:"3.00"
+        }
+    ]
+
+    
+}
